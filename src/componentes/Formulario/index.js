93ee077e093c1b8c33bc51f74a145b58f722b47a
Luciano Silva <lucianogs90@gmail.com>
2023-05-23 18:51:25 -0400

    criado o botao

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,17 +1,36 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const times = [
+        'Programacao',
+        'Front End',
+        'Data Science',
+        'Devops',
+        'Ux e Design',
+        'Mobile'
+    ]
+
+    const aoSalvar = (evento) => {
+      evento.preventDefault();
+      console.log('Form foi submetido')
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto obrigatorio={false} label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa obrigatorio={false} label='Time' itens={times} />
+                <Botao>Criar Card</Botao>
             </form>
         </section>
     )
 }
 
-export default Formulario
+export default Formulario;
